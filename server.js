36e110baa8f1308ddef0a703de251ff9f1a10ab3
SerAbinU1 <abinyt1234@gmail.com>
2025-09-10 10:30:53 +0000

    Set up server
    Gt06 and crc16 code taken from @ https://github.com/vondraussen/gt06

---
+++ server.js
@@ -0,0 +1,34 @@
+const net = require("node:net")
+
+const Gt06 = require("./gt06")
+
+const PORT = 5023
+
+const server = net.createServer((client) => {
+  console.log("gt06 connected: ", client.remoteAddress)
+
+  const gt06 = new Gt06()
+  client.on("data", (data) => {
+    console.log("recieved", data.toString("hex"))
+    try {
+      gt06.parse(data)
+    } catch (e) {
+      console.log("err", e)
+      return
+    }
+
+    if (gt06.expectsResponse) {
+      client.write(gt06.responseMsg)
+    }
+
+    gt06.msgBuffer.forEach((msg) => {
+      console.log(msg)
+    })
+
+    gt06.clearMsgBuffer()
+  })
+})
+
+server.listen(PORT, () => {
+  console.log("Server started on port: ", PORT)
+})
